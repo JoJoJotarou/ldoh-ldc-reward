06093b16505d75049ad350d98c8404777ff7396c
JoJoJotarou <58281079+JoJoJotarou@users.noreply.github.com>
2026-02-10 23:07:54 +0800

    fixed: debug 信息总是打印的问题

--- ldoh-new-api-helper.user.js
+++ ldoh-new-api-helper.user.js
@@ -64,16 +64,20 @@
         "color: inherit; font-weight: normal",
         ...args,
       ),
+    _printDebug: (level, msg, color, bg, ...args) =>
+      console.debug(
+        `%c LDOH %c ${level.toUpperCase()} %c ${msg}`,
+        "background: #6366f1; color: white; border-radius: 3px 0 0 3px; font-weight: bold; padding: 1px 4px",
+        `background: ${bg}; color: ${color}; border-radius: 0 3px 3px 0; font-weight: bold; padding: 1px 4px`,
+        "color: inherit; font-weight: normal",
+        ...args,
+      ),
     info: (msg, ...args) => Log._print("info", msg, "#fff", "#3b82f6", ...args),
     success: (msg, ...args) =>
       Log._print("ok", msg, "#fff", "#10b981", ...args),
     warn: (msg, ...args) => Log._print("warn", msg, "#000", "#f59e0b", ...args),
     error: (msg, ...args) => Log._print("err", msg, "#fff", "#ef4444", ...args),
-    debug: (msg, ...args) => {
-      //   if (localStorage.getItem("ldoh_debug")) {
-      Log._print("debug", msg, "#fff", "#8b5cf6", ...args);
-      //   }
-    },
+    debug: (msg, ...args) => Log._printDebug("debug", msg, "#fff", "#8b5cf6", ...args),
   };
 
   // ==================== 样式定义 ====================
